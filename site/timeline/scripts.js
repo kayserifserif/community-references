266e73545d8d42cfe09ed854082ea98280e73bf1
whykatherine <whykatherine@gmail.com>
2020-03-11 01:50:56 -0700

    feat: season stats

--- site/timeline/scripts.js
+++ site/timeline/scripts.js
@@ -3,8 +3,20 @@ var svg = d3.select("#container")
             .attr("width", "100%")
             .attr("height", "100%");
 
-d3.json("/output/referents.json").then(function(data) {
-  titles = Object.values(data.titles);
+var references;
+var referents;
+var people;
+var titles;
+
+// d3.json("/output/referents.json").then(function(data) {
+Promise.all([
+  d3.json("/output/references.json"),
+  d3.json("/output/referents.json")
+]).then(function(files) {
+  references = files[0];
+  referents = files[1];
+  people = Object.values(files[1].people);
+  titles = Object.values(files[1].titles);
 
   var timeline = svg
     // .data([1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020])
@@ -36,7 +48,7 @@ d3.json("/output/referents.json").then(function(data) {
       // .filter(function(d) { return d. })
       .on("mouseover", function(d) {
         d3.select(this).raise();
-        d3.select(this).style("opacity", 1);
+        // d3.select(this).style("opacity", 1);
         d3.select(this).classed("active", true);
       })
       .on("mouseout", function(d) {
@@ -50,17 +62,43 @@ d3.json("/output/referents.json").then(function(data) {
     // add title type
     this.classList.add(d.details.titleType);
     // add seasons
-    var seasons = [];
-    for (instance of d.references) {
-      var epCode = instance.epCode;
-      var season = epCode.slice(0, 3);
-      seasons.push(season);
-    }
-    for (season of seasons) {
-      this.classList.add(season);
+    for (season in d.countBySeason) {
+      if (d.countBySeason[season] > 0) {
+        this.classList.add(season);
+      }
     }
   });
   // dataPts.attr("data-legend", function(d) { return d.classList; });
+  
+  var bigBars = dataPts
+    .append("rect")
+    .classed("bigBar", true)
+    .attr("width", 5)
+    .attr("height", function(d) { return d.count * 30 })
+    .attr("transform", "translate(-2.5, 0)")
+    ;
+
+  var smallBars = dataPts
+    .append("rect")
+    .classed("smallBar", true)
+    .attr("width", 5)
+    .attr("height", function(d) {
+      var seasonChoice = document.querySelector("input:checked");
+      if (seasonChoice) {
+        var season = seasonChoice.value;
+        return d.countBySeason[season] * 30;
+      }
+      return 0;
+    })
+    .attr("transform", function(d) {
+      var seasonChoice = document.querySelector("input:checked");
+      if (seasonChoice) {
+        var season = seasonChoice.value;
+        return "translate(-2.5," + (d.count - d.countBySeason[season]) * 30 + ")";
+      }
+      return 0;
+    })
+    ;
 
   var bgs = dataPts
     .append("rect")
@@ -113,21 +151,89 @@ d3.json("/output/referents.json").then(function(data) {
     .attr("r", 5)
     .attr("transform", "translate(-10, -4)")
     ;
+
+  var seasonButtons = document.getElementsByName("seasonButton");
+  var dataPts = document.getElementsByClassName("dataPts");
+  for (button of seasonButtons) {
+    button.addEventListener("click", (event) => {
+      var season = event.target.value;
+
+      calculateSeasonStats(season);
+
+      for (pt of dataPts) {
+        if (pt.classList.contains(season)) {
+          var references = pt.__data__.references;
+          var countInSeason = 0;
+          for (instance of references) {
+            if (instance.epCode.slice(0, 3) == season) {
+              countInSeason++;
+            }
+          }
+          var startYear = pt.__data__.details.startYear;
+          var x1 = (startYear - 1930) * 16;
+          var y = window.innerHeight - 50;
+          y -= countInSeason * 30;
+          pt.style.transform = "translate(" + x1 + "," + y + ")";
+          pt.style.opacity = Math.min(countInSeason / 23 + 0.5, 1);
+        } else {
+          pt.style.opacity = 0;
+        }
+      }
+
+      smallBars
+        .attr("height", function(d) {
+          var season = document.querySelector("input:checked").value;
+          var countInSeason = 0;
+          for (instance of d.references) {
+            var epCode = instance.epCode;
+            if (epCode.slice(0, 3) == season) {
+              countInSeason++;
+            }
+          }
+          return countInSeason * 30;
+        })
+        .attr("transform", function(d) {
+          var season = document.querySelector("input:checked").value;
+          var countInSeason = 0;
+          for (instance of d.references) {
+            var epCode = instance.epCode;
+            if (epCode.slice(0, 3) == season) {
+              countInSeason++;
+            }
+          }
+          return "translate(-2.5," + (d.count - countInSeason) * 30 + ")";
+        })
+        ;
+    });
+  }
 });
 
-var seasonButtons = document.getElementsByName("seasonButton");
-var dataPts = document.getElementsByClassName("dataPts");
-for (button of seasonButtons) {
-  button.addEventListener("click", (event) => {
-    var value = event.target.value;
-    for (pt of dataPts) {
-      if (pt.classList.contains(value)) {
-        var count = pt.__data__.count;
-        pt.style.opacity = Math.min(count / 23 + 0.5, 1);
-      } else {
-        // pt.style.display = "none";
-        pt.style.opacity = 0;
-      }
+function calculateSeasonStats(value) {
+  var numRefs = 0;
+  var numPeopleRefs = 0;
+  var numTitleRefs = 0;
+  for (name in referents["people"]) {
+    var ent = referents["people"][name];
+    if (ent["countBySeason"][value] > 0) {
+      numRefs++;
+      numPeopleRefs++;
     }
-  });
+  }
+  for (title in referents["titles"]) {
+    var ent = referents["titles"][title];
+    if (ent["countBySeason"][value] > 0) {
+      numRefs++;
+      numTitleRefs++;
+    }
+  }
+  // for (refType of Object.values(referents)) {
+  //   for (entity in refType) {
+  //     if (refType[entity]["countBySeason"][value] > 0) {
+  //       numRefs++;
+  //     }
+  //   }
+  // }
+  document.getElementById("numRefs").textContent = numRefs;
+  document.getElementById("numPeopleRefs").textContent = numPeopleRefs;
+  document.getElementById("numTitleRefs").textContent = numTitleRefs;
 }
