a5d1bf76747b1e178cbfe5253e20740362c8a616
whykatherine <whykatherine@gmail.com>
2020-03-22 00:40:20 +0800

    chore: delete files

--- site/assets/scripts.js
+++
@@ -1,8 +0,0 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//   var references = document.getElementsByClassName("reference");
-//   for (ref of references) {
-//     ref.addEventListener("mouseover", (event) => {
-//       console.log(event, event.target);
-//     });
-//   }
-// });
