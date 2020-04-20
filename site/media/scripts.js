6e40e4aa141e6f0372af3ad1e2340dc1c7a5717d
whykatherine <whykatherine@gmail.com>
2020-04-20 21:25:12 +0800

    feat: by media

---
+++ site/media/scripts.js
@@ -0,0 +1,73 @@
+Promise.all([
+  d3.json("/output/references.json"),
+  d3.json("/output/referents.json")
+]).then(function(files) {
+  references = files[0];
+  referents = files[1];
+  people = Object.values(files[1].people);
+  titles = Object.values(files[1].titles);
+  console.log(people);
+  console.log(titles);
+
+  people.forEach(d => {
+    let list = d3.select("#people");
+    let div = list.append("div");
+    div.append("dt").text(d.details.name).classed("entity", true);
+    let def = div.append("dd");
+    let epRefs = {};
+    d.references.forEach(ref => {
+      if (epRefs[ref.epCode]) {
+        epRefs[ref.epCode].push(ref);
+      } else {
+        epRefs[ref.epCode] = [ref];
+      }
+    });
+    for ([epCode, refs] of Object.entries(epRefs)) {
+      let subList = def.append("dl");
+      let ep = subList.append("dt").text(epCode);
+      refs.forEach(ref => {
+        let sent = subList.append("dd");
+        let startInSent = ref.startInSent;
+        let endInSent = ref.endInSent;
+        let markedSent = ref.sentence.substring(0, startInSent) +
+                         "<span class='entity'>" +
+                         ref.sentence.substring(startInSent, endInSent) + 
+                         "</span>" +
+                         ref.sentence.substring(endInSent, ref.sentence.length);
+        sent.html(markedSent);
+      });
+    };
+  });
+
+  titles.forEach(d => {
+    let list = d3.select("#titles");
+    let div = list.append("div");
+    div.append("dt").text(d.details.title).classed("entity", true);
+    let def = div.append("dd");
+    let epRefs = {};
+    d.references.forEach(ref => {
+      if (epRefs[ref.epCode]) {
+        epRefs[ref.epCode].push(ref);
+      } else {
+        epRefs[ref.epCode] = [ref];
+      }
+    });
+    for ([epCode, refs] of Object.entries(epRefs)) {
+      let subList = def.append("dl");
+      let ep = subList.append("dt").text(epCode);
+      refs.forEach(ref => {
+        let sent = subList.append("dd");
+        let startInSent = ref.startInSent;
+        let endInSent = ref.endInSent;
+        let markedSent = ref.sentence.substring(0, startInSent) +
+                         "<span class='entity'>" +
+                         ref.sentence.substring(startInSent, endInSent) + 
+                         "</span>" +
+                         ref.sentence.substring(endInSent, ref.sentence.length);
+        sent.html(markedSent);
+      });
+    };
+  });
+
+
+});
