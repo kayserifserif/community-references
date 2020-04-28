caa61207c9f66ddc9088eb35ffef1295035cebc2
whykatherine <whykatherine@gmail.com>
2020-04-28 16:25:28 +0800

    feat: minimise ratings db

--- scripts/dbs.py
+++ scripts/dbs.py
@@ -8,16 +8,29 @@ def readDb(file):
   try:
     with open(file, "r", encoding="ISO-8859-1") as f:
       reader = csv.DictReader(f, dialect="excel-tab")
-      db = []
+      # db = []
+      db = {}
       print(f"Reading {file}...")
       start = time.time()
-      if "name" in file:
+      if "name.basics" in file:
         for row in reader:
           db.append(dict(row))
           print("Row: {:7}".format(len(db)), end="\r")
-      else:
+      elif "title.basics" in file:
         for row in reader:
-          db.append(row)
+          # if row["runtimeMinutes"].isdigit():
+            # row["runtimeMinutes"] = int(row["runtimeMinutes"])
+          # db.append(row)
+          tconst = row["tconst"]
+          row.pop("tconst", None)
+          db[tconst] = row
+          print("Row: {:7}".format(len(db)), end="\r")
+      elif "title.ratings.tsv" in file:
+        for row in reader:
+          # db.append(row)
+          tconst = row["tconst"]
+          row.pop("tconst", None)
+          db[tconst] = row
           print("Row: {:7}".format(len(db)), end="\r")
       end = time.time()
       delta = end - start
@@ -30,11 +43,19 @@ def readDb(file):
 def writeDb(db, file):
   try:
     with open(file, "w", encoding="ISO-8859-1") as f:
-      writer = csv.DictWriter(f, fieldnames=db[0].keys(), dialect="excel-tab")
+      # writer = csv.DictWriter(f, fieldnames=db[0].keys(), dialect="excel-tab")
+      fields = ["tconst", "averageRating", "numVotes"]
+      writer = csv.DictWriter(f, fieldnames=fields, dialect="excel-tab")
       writer.writeheader()
       print(f"Writing to {file}...")
-      for row in db:
-        writer.writerow(row)
+      # for row in db:
+        # writer.writerow(row)
+      for tconst in db:
+        writer.writerow({
+          "tconst": tconst,
+          "averageRating": db[tconst]["averageRating"],
+          "numVotes": db[tconst]["numVotes"]
+          })
       print(f"Successfully saved to {file}!")
   except IOError:
     print(f"Could not write to {file}.")
@@ -49,7 +70,7 @@ def keepEntry(entry, condsToRemove, mustMatchAll):
     if cond[2] == 0:
       match = entry[cond[0]] == cond[1]
     elif cond[2] == -1:
-      if cond[1].isdigit():
+      if type(cond[1]) == int:
         try:
           match = int(entry[cond[0]]) < int(cond[1])
         except ValueError:
@@ -57,7 +78,7 @@ def keepEntry(entry, condsToRemove, mustMatchAll):
       else:
         match = len(entry[cond[0]]) < len(cond[1])
     elif cond[2] == 1:
-      if cond[1].isdigit():
+      if type(cond[1]) == int:
         try:
           match = int(entry[cond[0]]) > int(cond[1])
         except ValueError:
@@ -89,10 +110,23 @@ def removeEntries(db, condsToRemove, mustMatchAll=True):
   print(f"Removed {str(len(db) - len(newDb))} entries in {'{0:.2f}'.format(delta)} seconds.")
   return newDb
 
+def constInDb(db, const):
+  constType = const[0] + "const"
+  return const in [y[constType] for y in db]
+
+def intersect(db, dbToMatch, key):
+  print(f"Removing entries in `db` that aren't in `dbToMatch` according to `key`...")
+  start = time.time()
+  newDb = {key : value for (key, value) in db.items() if key in dbToMatch}
+  end = time.time()
+  delta = end - start
+  print(f"Removed {str(len(db) - len(newDb))} entries in {'{0:.2f}'.format(delta)} seconds.")
+  return newDb
+
 def main():
   # # titlesFile = "./db/title.basics.tsv"
-  # titlesFile = "./db/title.basics.min.tsv"
-  # titles = readDb(titlesFile)
+  titlesFile = "./db/title.basics.min.tsv"
+  titles = readDb(titlesFile)
   # print("Titles: " + "{:7}".format(len(titles)) + " rows")
   # titles = removeEntries(titles, [
   #   ("titleType", "short", 0),
@@ -103,9 +137,12 @@ def main():
   # ], mustMatchAll=False) # remaining: movie, tvMovie, tvSeries, tvMiniSeries, tvSpecial
   # titles = removeEntries(titles, [("isAdult", "1", 0)])
   # titles = removeEntries(titles, [("startYear", "\\N", 0), ("endYear", "\\N", 0)])
+  # titles = removeEntries(titles, [("titleType", "movie", 0), ("runtimeMinutes", "\\N", 0)])
+  # titles = removeEntries(titles, [("titleType", "tvSeries", 0), ("runtimeMinutes", "\\N", 0)])
   # titles = removeEntries(titles, [("titleType", "movie", 0), ("runtimeMinutes", 60, -1)])
   # titles = removeEntries(titles, [("titleType", "tvSeries", 0), ("runtimeMinutes", 60, -1)])
   # titles = removeEntries(titles, [("genres", "Biography", 2)])
+  # titles = removeEntries(titles, [("genres", "\\N", 0)])
   # print("Titles: " + "{:7}".format(len(titles)) + " rows")
   # writeDb(titles, "./db/title.basics.min.tsv")
   
@@ -133,7 +170,14 @@ def main():
   # print("Names: " + "{:7}".format(len(names)) + " rows")
   # writeDb(names, "./db/name.basics.min.tsv")
   
-  return
+  ratingsFile = "./db/title.ratings.tsv"
+  ratings = readDb(ratingsFile)
+  print("Ratings: " + "{:7}".format(len(ratings)) + " rows")
+  ratings = intersect(ratings, titles, "tconst")
+  print("Ratings: " + "{:7}".format(len(ratings)) + " rows")
+  writeDb(ratings, "./db/title.ratings.min.tsv")
+
+  # return
 
 if __name__ == "__main__":
   main()
