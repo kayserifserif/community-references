113a1df8945cd04ce43680dfd34f92c34084a8ac
whykatherine <whykatherine@gmail.com>
2020-11-08 23:17:14 +0800

    use videogrep python module

--- py/supercut.py
+++ py/supercut.py
@@ -1,16 +1,48 @@
 import sys
 import re
+import json
 from videogrep import videogrep
 
+def getReferences():
+  file = "./data/community/references.json"
+  try:
+    with open(file) as f:
+      references = json.load(f)
+    return references
+  except IOError:
+    print(f"Could not read {file}.")
+    return
+
+def compileRefsSet(references, code):
+  search = set()
+  for refType in references[code]:
+    for ref in references[code][refType]:
+      entity = re.escape(ref["reference"]["entity"])
+      search.add(entity)
+  return search
+
+def compileRefs(code):
+  references = getReferences()
+  search = set()
+  if re.match(r"s\d{2}e\d{2}", code):
+    search = search | compileRefsSet(references, code)
+  elif re.match(r"s\d{2}", code):
+    epCodes = sorted([f[:-4] for f in listdir("./transcripts") if isfile(join("./transcripts", f)) and code in f])[1:]
+    for epCode in epCodes:
+      search = search | compileRefsSet(references, epCode)
+  searchStr = "(" + "|".join(search) + ")"
+  print(searchStr)
+  return searchStr
+
 def create_supercut(epCode):
-  epCode = epCode.upper()
-  inFile = ["video/community/Community-" + epCode[:3] + "/Community-" + epCode + ".mkv"]
-  search = "(Marty\\ McFly|Edward\\ Scissorhands|Ricardo\\ Montalban|Dolly\\ Parton|E\\.T\\.)"
+  epCode = epCode
+  inFile = ["video/community/" + epCode[:3] + "/" + epCode + ".mkv"]
+  search = compileRefs(epCode.lower())
   outFile = "video/supercuts/supercut-" + epCode + ".mp4"
   videogrep(inFile, outFile, search, "re")
 
 def main():
-  if len(sys.argv) == 2 and re.match(r"s\d{2}", sys.argv[1]):
+  if len(sys.argv) == 2 and re.match(r"s\d{2}e\d{2}", sys.argv[1]):
     create_supercut(sys.argv[1])
   else:
     print("usage: \n\
